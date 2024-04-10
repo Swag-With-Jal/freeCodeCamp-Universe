25d36a49bad2fec2e076dee66b622825e7bd0314
Swag-With-Jal <141929555+Swag-With-Jal@users.noreply.github.com>
2024-04-10 07:51:39 +0530

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -9,32 +9,33 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-echo $($PSQL "TRUNCATE teams, games");
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-  if [[ $YEAR != "year" ]]
-  then
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'"); 
-    if [[ -z $WINNER_ID ]]
-    then
-      $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')");
-      # WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
+
+# Function to get team ID from team name
+get_team_id() {
+  local team_name="$1"
+  local team_id=$($PSQL "SELECT team_id FROM teams WHERE name='$team_name'")
+  echo "$team_id"
+}
+
+# Main script
+echo $($PSQL "TRUNCATE teams, games")
+tail -n +2 games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
+  if [[ $YEAR != "year" ]]; then
+    # Get or insert winner ID
+    WINNER_ID=$(get_team_id "$WINNER")
+    if [[ -z $WINNER_ID ]]; then
+      $PSQL "INSERT INTO teams(name) VALUES('$WINNER') ON CONFLICT DO NOTHING"
+      WINNER_ID=$(get_team_id "$WINNER")
     fi
-    
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams where name='$OPPONENT'");
-    if [[ -z $OPPONENT_ID ]]
-    then
-      $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')");
-      # OPPONENT_ID=$($PSQL "SELECT team_id FROM teams where name='$OPPONENT'");
-    fi  
 
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
-    $($PSQL "INSERT INTO games(year, round, winner_goals, opponent_goals, winner_id, opponent_id) VALUES($YEAR, '$ROUND', $WINNER_GOALS, $OPPONENT_GOALS, $WINNER_ID, $OPPONENT_ID)");
-  
+    # Get or insert opponent ID
+    OPPONENT_ID=$(get_team_id "$OPPONENT")
+    if [[ -z $OPPONENT_ID ]]; then
+      $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') ON CONFLICT DO NOTHING"
+      OPPONENT_ID=$(get_team_id "$OPPONENT")
+    fi
+
+    # Insert game data
+    $PSQL "INSERT INTO games(year, round, winner_goals, opponent_goals, winner_id, opponent_id) VALUES($YEAR, '$ROUND', $WINNER_GOALS, $OPPONENT_GOALS, $WINNER_ID, $OPPONENT_ID)"
   fi
-done
-
-cat gt.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
 done
